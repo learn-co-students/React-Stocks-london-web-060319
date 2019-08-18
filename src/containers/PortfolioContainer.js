d8b9621b6aebe1327d3723a40e1edc205b6292c8
Federica Macchi <federica.macchi3@gmail.com>
2019-08-18 14:46:22 +0100

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
-import Stock from '../components/Stock'
+import React, { Component } from "react";
+import Stock from "../components/Stock";
 
 class PortfolioContainer extends Component {
-
   render() {
-    return (
-      <div>
-        <h2>My Portfolio</h2>
-          {
-            //render your portfolio stocks here
-          }
-      </div>
-    );
+    let {stocks, sellStock} = this.props;
+    
+    if (stocks !== undefined) {
+      return (
+        <div>
+          <h2>My Portfolio</h2>
+          {stocks.map(stock => (
+            <Stock key={stock.id} stock={stock} handleClick={sellStock}/>
+          ))}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h2>My Portfolio</h2>
+        </div>
+      );
+    }
   }
-
 }
 
 export default PortfolioContainer;
