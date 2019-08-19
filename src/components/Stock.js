5188aa6483411d99cbd9589702352a3bcd4b7b23
Amy Quinn <amyequinn@hotmail.co.uk>
2019-08-19 18:19:51 +0100

    Done.

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,21 +1,26 @@
 import React from 'react'
 
-const Stock = () => (
+const Stock = (props) => {
+    const { stock } = props;
+    return(
   <div>
 
-    <div className="card">
+    <div className="card"
+    onClick={props.handleClick}>
       <div className="card-body">
         <h5 className="card-title">{
-            //Company Name
+          stock.name
           }</h5>
-        <p className="card-text">{
-            //ticker: stock price
-          }</p>
+        <p className="card-text">
+          {stock.ticker} {stock.price}
+
+          </p>
       </div>
     </div>
 
 
   </div>
-);
+)
+};
 
 export default Stock
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,22 +3,97 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+const stocksAPI = 'http://localhost:3000/stocks'
+
+  const getStocks = () =>
+    fetch(stocksAPI)
+    .then(resp => resp.json())
+
+
 class MainContainer extends Component {
 
+  state = {
+    stocks: [],
+    selectedStockIds: [],
+    filterChoice: "None",
+    sortChoice: ""
+  }
+
+
+  componentDidMount(){
+     getStocks()
+    .then(stocks =>this.setState({stocks}))
+  }
+
+  selectedStock = stock => {
+    if (this.state.selectedStockIds.includes(stock.id)) return;
+    this.setState({
+      selectedStockIds: [...this.state.selectedStockIds, stock.id]
+    })
+  }
+
+
+removeStock = stock => {
+  this.setState({
+  selectedStockIds: this.state.selectedStockIds.filter(selectedStockId => selectedStockId !==stock.id)
+})
+}
+
+
+getSelectedStock = () =>
+  this.state.selectedStockIds.map(
+    selectedStockId =>
+    this.state.stocks.find(stock => stock.id === selectedStockId))
+
+
+filterStocks = () => {
+  return this.state.stocks.filter(stock => {
+    if(this.state.filterChoice === "None") return true;
+    if (this.state.filterChoice === "Tech") return stock.type === "Tech";
+    if (this.state.filterChoice === "Sportswear") return stock.type === "Sportswear";
+    if (this.state.filterChoice === "Finance") return stock.type === "Finance";
+  })
+}
+
+changeFilterChoice = event => {
+  this.setState({filterChoice: event.target.value});
+}
+
+
+sortStocks = (filteredStocks) => {
+  return filteredStocks.sort((stockA, stockB) => {
+    if (this.state.sortChoice === "") return 0;
+    if (this.state.sortChoice === "Alphabetically") {return (stockA.ticker.localeCompare(stockB.ticker))}
+    if (this.state.sortChoice === "Price") {return (stockA.price - stockB.price)}
+  })
+}
+
+changeSortChoice = event => {
+  this.setState({ sortChoice: event.target.value})
+
+}
+
   render() {
+    const filteredStocks = this.filterStocks();
+    const sortedStocks = this.sortStocks(filteredStocks)
     return (
       <div>
-        <SearchBar/>
+        <SearchBar
+        changeFilterChoice={this.changeFilterChoice}
+        filterChoice={this.state.filterChoice}
+        changeSortChoice={this.changeSortChoice}
+        sortChoice={this.state.sortChoice}
+        />
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={sortedStocks} selectedStock={this.selectedStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={this.getSelectedStock()} removeStock={this.removeStock}/>
 
             </div>
           </div>
