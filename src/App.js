d4905dac788bb422b84124bc9f0c7d39984f934f
Jowita <jowita.lapies@gmail.com>
2019-08-21 21:52:00 +0100

    Done.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,27 @@ import React, { Component } from 'react';
 import Header from './components/Header'
 import MainContainer from './containers/MainContainer'
 
+const baseUrl = "http://localhost:3000/stocks"
 class App extends Component {
+  
+  state = {
+    stocks: []
+  }
+
+  componentDidMount(){
+    fetch(baseUrl)
+    .then(resp => resp.json())
+    .then(data => {
+      this.setState({
+        stocks: data
+      })
+    })
+  }
   render() {
     return (
       <div>
         <Header/>
-        <MainContainer/>
+        <MainContainer stocks={this.state.stocks}/>
       </div>
     );
   }
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -3,22 +3,69 @@ import StockContainer from './StockContainer'
 import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
+
 class MainContainer extends Component {
+  state = {
+    portfolio: [],
+    search: "",
+    filterType: ""
+  }
+
+  addStockToPortfolio = (stock) => {
+    if (!this.state.portfolio.includes(stock)){
+      this.setState({
+        portfolio: [...this.state.portfolio, stock]
+      })
+    }
+  }
+
+  handleChange = event =>{
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+  
+  handleType = event => {
+    this.setState({
+      filterType: event.target.value
+    })
+  }
+
+  filterSearch = () => {
+    if (this.state.search){
+      return this.props.stocks.filter(stock => stock.name.toLowerCase().includes(this.state.search.toLowerCase()))
+    }
+    return this.props.stocks
+  }
+  
+  filterCategory = () => {
+    if (this.state.filterType){
+      return this.props.stocks.filter(stock => stock.type === this.state.filterType)
+    }
+    return this.props.stocks
+  }
+
+  removeStockFromPortfolio = stock => {
+    this.setState({
+      portfolio: this.state.portfolio.filter(sellStock => sellStock.id !== stock.id)
+    })
+    
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar handleType={this.handleType} handleChange={this.handleChange} />
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer addStockToPortfolio={this.addStockToPortfolio} stocks={this.filterSearch()}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer removeStockFromPortfolio={this.removeStockFromPortfolio} stocks={this.state.portfolio}/>
 
             </div>
           </div>
